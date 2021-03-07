be2380e02665b0b022f73f1ec1e5aa9f82bb3f88
yuzhu-noodles <qli@snnu.edu.cn>
2021-03-07 21:20:38 +0800

    字符串转换成整数

---
+++ StrToInt.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int StrToInt(string str) 
+	{
+		if (str.empty())
+			return 0;
+		int flag = 1;
+		if (str[0] == '+')
+		{
+			flag = 1;
+			str[0] = '0';
+		}
+		if (str[0] == '-')
+		{
+			flag = -1;
+			str[0] = '0';
+		}
+		int sum = 0;
+		for (int i = 0; i < str.size(); i++)
+		{
+			if (str[i] < '0' || str[i]>'9')
+			{
+				sum = 0;
+				break;
+			}
+			sum = sum * 10 + str[i] - '0';
+		}
+		return flag*sum;
+    }
+};
+
+int main()
+{
+	string str;
+	cin >> str;
+	Solution S;
+	cout<<S.StrToInt(str);
+	system("pause");
+	return 0;
+}
